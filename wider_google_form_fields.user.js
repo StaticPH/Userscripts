ebf9f2b22ecdc60c8624db1ddef95d775959c093
StaticPH <7786502+StaticPH@users.noreply.github.com>
2022-08-19 03:03:43 -0400

    Replace fallback for GM.addStyle with something that should be closer to the actual implementation being substituted for.

--- wider_google_form_fields.user.js
+++ wider_google_form_fields.user.js
@@ -2,7 +2,7 @@
 // @name        Wider Google Form Fields
 // @namespace   https://github.com/StaticPH
 // @match       https://docs.google.com/forms/*
-// @version     1.0
+// @version     1.1
 // @createdAt   9/30/2021, 1:34:35 PM
 // @author      StaticPH
 // @description Widens the input fields in google forms from 50% to 100% of the question element (minus padding)
@@ -20,11 +20,20 @@
 
 (function(){
 	"use strict";
-	if (!(GM && GM.addStyle)){
-		console.log('GM.addStyle is not defined. Falling back to GM_addStyle.');
-		GM = GM ? GM : {};
-		GM.addStyle = GM.addStyle ? GM.addStyle : GM_addStyle;
+
+	if (typeof GM == 'undefined'){
+		this.GM = {};
 	}
+	if (typeof GM['addstyle'] == 'undefined'){
+		console.log('GM.addStyle is not defined. Falling back to GM_addStyle Promise.');
+		GM['addStyle'] = function(...args){
+			return new Promise((onResolve, onReject) => {
+				try{ onResolve(GM_addStyle.apply(this, args)); }
+				catch(err){ onReject(err); }
+			});
+		}
+	}
+
 	GM.addStyle(`
 	.freebirdFormviewerComponentsQuestionTextShort.freebirdFormviewerComponentsQuestionTextTextInput{
 		width: 100%;
