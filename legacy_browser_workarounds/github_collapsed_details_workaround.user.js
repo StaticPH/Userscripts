24ff06d73ade76cfd8c04279021f82a69c4692cc
StaticPH <7786502+StaticPH@users.noreply.github.com>
2023-03-12 22:18:23 -0400

    Add basic support for GitHub Action runs to legacy_browser_workarounds/github_collapsed_details_workaround.user.js; implemented more than three months ago, but forgotten in stash. Oops.

--- legacy_browser_workarounds/github_collapsed_details_workaround.user.js
+++ legacy_browser_workarounds/github_collapsed_details_workaround.user.js
@@ -2,7 +2,7 @@
 // @name           GitHub Collapsed Details Legacy Workaround
 // @namespace      https://github.com/StaticPH
 // @match          https://github.com/*/*
-// @version        1.1
+// @version        1.2
 // @createdAt      9/30/2022, 9:32:58 PM
 // @author         StaticPH
 // @description    Add simple onclick handlers to the collapsed details of commits on GitHub, as the normal behavior of expanding the ellipses to the full commit message when clicked seems to have broken on legacy browsers as a result of some change to the implementation. Also fixes some other instances of non-functioning collapsing elements.
@@ -21,8 +21,9 @@
 	"use strict";
 
 	let hasRefinedGithub = document.querySelector('.refined-github') !== null;
+	const urlRelativeToRepoDefault = '/' + document.location.pathname.split('/').slice(3).join('/');
 	const parser = new DOMParser();
-	const requestTemplate = {
+	const requestTemplateHTML = {
 		'credentials': 'include',
 		'headers': {
 			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9',
@@ -45,20 +46,55 @@
 
 	document.querySelectorAll('.hidden-text-expander > button.ellipsis-expander.js-details-target').forEach(expander => expander.onclick = toggleShowDetails);
 
-	// Fix collapsed categories on wiki page sidebar.
-	document.querySelectorAll('[data-view-component] include-fragment[loading="lazy"][src]:not([src=""])').forEach(async function(lazyPageFrag){
-		try{
-			await fetch(lazyPageFrag.getAttribute('src'), requestTemplate).then(
-				resp => resp.text().then(
-					async function(html){
-						await lazyPageFrag.replaceChildren(...parser.parseFromString(html, 'text/html').querySelectorAll('body > ul'));
-						await lazyPageFrag.querySelectorAll('[data-analytics-event]').forEach(e => e.removeAttribute('data-analytics-event'));
-					},
-					err => console.warn('There was a problem retrieving the response text', err)
-				),
-				err => console.warn('Call to fetch yielded a rejected Promise.', err)
-			);
+	if (urlRelativeToRepoDefault.startsWith('/wiki')){
+		// Fix collapsed categories on wiki page sidebar.
+		document.querySelectorAll('[data-view-component] include-fragment[loading="lazy"][src]:not([src=""])').forEach(async function(lazyPageFrag){
+			try{
+				await fetch(lazyPageFrag.getAttribute('src'), requestTemplateHTML).then(
+					resp => resp.text().then(
+						async function(html){
+							await lazyPageFrag.replaceChildren(...parser.parseFromString(html, 'text/html').querySelectorAll('body > ul'));
+							await lazyPageFrag.querySelectorAll('[data-analytics-event]').forEach(e => e.removeAttribute('data-analytics-event'));
+						},
+						err => console.warn('There was a problem retrieving the response text', err)
+					),
+					err => console.warn('Call to fetch yielded a rejected Promise.', err)
+				);
+			}
+			catch(err){ console.warn(err); } // In the event that any individual fetch call-chain has trouble, just print a warning and continue to finish any remaining fetch quests.
+		});
+	}
+	else if (urlRelativeToRepoDefault.startsWith('/actions/runs')){
+		// Fix "Show more"/"Show less" buttons on job run annotations
+		const selectorToggleFullAnnotationMessage = 'annotation-message > div > div > [data-action="click:annotation-message#toggleExpansion"]';
+		const selectorAnnotationBody = '[data-target="annotation-message.annotationContainer"]';
+		const selectorAnnotationBodyAlternative = '.annotation--contracted, .annotation--expanded';
+
+		function onToggleFullAnnotationBody(evnt){
+			// Return early if the event target isn't relevant, i.e. not a "Show more"/"Show less" button on a job run annotation.
+			if (!evnt.target.matches(selectorToggleFullAnnotationMessage)){ return; }
+			const annotationBody = evnt.target.parentElement.querySelector(selectorAnnotationBody) || evnt.target.parentElement.querySelector(selectorAnnotationBodyAlternative);
+			if (!annotationBody){
+				// Print a warning to the console.
+				// Doesn't mention the alternative selector because "annotation--expanded" is just an arbitrarily chosen string,
+				// and doesn't necessarily match the normal behavior.
+				console.warn('Expected annotation-message to contain a child with the "data-target" attribute set to "annotation-message.annotationContainer"');
+				return;
+			}
+			if (annotationBody.classList.contains('annotation--contracted')){
+				annotationBody.classList.replace('annotation--contracted', 'annotation--expanded');
+				evnt.target.toggleAttribute('hidden');
+				evnt.target.parentElement.querySelector('[data-target="annotation-message.showLessButton"]').toggleAttribute('hidden');
+			}
+			else { // Normally implies that classList contains 'annotation--expanded'
+				// replace 'annotation--expanded' with 'annotation--contracted' if the former exists, otherwise just add the latter;
+				// the second case is a fallback in case the class was previously just removed entirely, rather than replaced.
+				annotationBody.classList.replace('annotation--expanded', 'annotation--contracted') || annotationBody.classList.add('annotation--contracted');
+				evnt.target.toggleAttribute('hidden');
+				evnt.target.parentElement.querySelector('[data-target="annotation-message.showMoreButton"]').toggleAttribute('hidden');
+			}
 		}
-		catch(err){ console.warn(err); } // In the event that any individual fetch call-chain has trouble, just print a warning and continue to finish any remaining fetch quests.
-	});
+		// Add global (document) scoped event listener for click events.
+		document.addEventListener('click', onToggleFullAnnotationBody);
+	}
 })();
