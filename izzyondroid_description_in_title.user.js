e7e9030638f95439bc71f7092558b70756c3f786
StaticPH <7786502+StaticPH@users.noreply.github.com>
2024-05-02 17:33:00 -0400

    Add new userscript: izzyondroid_description_in_title.user.js

---
+++ izzyondroid_description_in_title.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name             Better IzzyOnDroid App Titles
+// @namespace        https://github.com/StaticPH
+// @match            https://apt.izzysoft.de/fdroid/index/apk/*
+// @version          1.0.0
+// @createdAt        5/2/2024, 5:33:04 PM
+// @author           StaticPH
+// @description      Adds app description to page titles where possible.
+// @license          MIT
+// @updateURL        https://raw.githubusercontent.com/StaticPH/Userscripts/master/izzyondroid_description_in_title.user.js
+// @downloadURL      https://raw.githubusercontent.com/StaticPH/Userscripts/master/izzyondroid_description_in_title.user.js
+// @homepageURL      https://github.com/StaticPH/UserScripts
+// @supportURL       https://github.com/StaticPH/UserScripts/issues
+// @icon             https://apt.izzysoft.de/fdroid/izzy-on-droid.png
+// @grant            none
+// @run-at           document-end
+// @noframes
+// ==/UserScript==
+
+(function(){
+	"use strict";
+
+	const parts = document.title.split('- IzzyOnDroid');
+	const tail = parts.pop();
+	let summary = document.getElementById('summary').textContent;
+	summary = summary[0].toLocaleUpperCase() + summary.slice(1);
+	parts.push('- ' + summary + ' | IzzyOnDroid' + tail);
+	document.title = parts.join('');
+})();
