6c8a404992c3a445bb1223f3f3ccbcdbf0dc9589
StaticPH <7786502+StaticPH@users.noreply.github.com>
2025-07-12 19:35:58 -0400

    Add help output for the shell script that generates updated the readme and manifest files.

--- build/run.sh
+++ build/run.sh
@@ -1,15 +1,31 @@
 #! /bin/sh
 
+# To "register" a new userscript, add a new object with `path` and `anchorString` values to the `scripts` array in `main_script_manifest.json`; the updater python script will do the rest.
+case "$1" in
+	-h|--help)
+		printf '%s\n' \
+			'This script is used to generate updated versions of readme and manifest files.' \
+			'To "register" a new userscript for inclusion in the readme, add a new'  \
+			'object with both `path` and `anchorString` values to the `scripts` array' \
+			'in `main_script_manifest.json` before running this script.' \
+			'Updated manifest and readme files will be created in the `build` directory,' \
+			'and named `newdata.json` and `output.md`, respectively.' \
+			'They can be manually copied over the originals.'
+			exit 0
+		;;
+esac
+
 repoRoot="$(git rev-parse --show-toplevel)"
 dataDir="${repoRoot}/build/data_files"
 templateDir="${repoRoot}/build/templates"
 
-# To "register" a new userscript, add a new object with `path` and `anchorString` values to the `scripts` array in `main_script_manifest.json`; the updater python script will do the rest.
+pushd "${repoRoot}/build" >/dev/null 2>&1
 
 TEST_READER=1 python3 "${repoRoot}/build/update_script_manifest.py"
 jq -rs 'reduce .[] as $item ({}; . * $item)' "${dataDir}/general_url_references.json" "${dataDir}/legacy_scripts.json" newdata.json | \
 	minijinja-cli --format json "${templateDir}/primary_template.md.j2" - > output.md
 
+popd >/dev/null 2>&1
 ################
 ################
 # TODO:
